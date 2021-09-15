878ca0e42183d0d598b5de2bfcb1a30263f194f3
dsneddon00 <dsneddon00@gmail.com>
2021-09-15 03:20:52 -0600

    Added several database methods, all stress tested.

--- EmpireDB.py
+++ EmpireDB.py
@@ -20,3 +20,27 @@ class EmpireDB:
         self.cursor.execute("SELECT * FROM empire")
         data = self.cursor.fetchall()
         return data
+
+    def getOneFromEmpire(self, id):
+        data = [id]
+        self.cursor.execute("SELECT * FROM empire WHERE id = ?", data)
+        result = self.cursor.fetchone()
+        return result
+
+    def createLog(self, name):
+        data = [name]
+        self.cursor.execute("INSERT INTO empire (name) VALUES (?)", data)
+        self.connection.commit()
+        return
+
+    def updateLog(self, id, name):
+        data = [name, id]
+        self.cursor.execute("UPDATE empire SET name = ? WHERE id = ?", data)
+        self.connection.commit()
+        return
+
+    def deleteLog(self, id):
+        data = [id]
+        self.cursor.execute("DELETE FROM empire SET name = ?", data)
+        self.connection.commit()
+        return
--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import EmpireDB
 
 def main():
     db = EmpireDB.EmpireDB()
-    print(db.getAllFromEmpire()[0]["name"])
-
+    #print("Test getAllFromEmpire:", db.getAllFromEmpire()[0]["name"])
+    #print("Test getOneFromEmpire:", db.getOneFromEmpire(1))
+    db.updateLog(1, "Gamer")
+    #db.createLog("Testing")
+    #print("Test getAllFromEmpire:", db.getAllFromEmpire()[0]["name"])
+    #print("Test getOneFromEmpire:", db.getOneFromEmpire(2))
+    print(db.getAllFromEmpire())
 
 if __name__ == "__main__":
     main()
