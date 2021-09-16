ca2745cc13d33b3e0ef19b82389093fe902b2a02
dsneddon00 <dsneddon00@gmail.com>
2021-09-16 16:55:07 -0600

    Added the pickle system, need to transfer it over to Nitrogren.

---
+++ pickleStorage.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import pickle
+
+if os.path.exists("sectors.p"):
+    fin = open("sectors.p", "rb")
+    sectors = pickle.load(fin)
+    fin.close()
+else:
+    sectors = {}
+
+for line in sys.stdin.readlines():
+    line = line.strip()
+    words = line.split()
+    if len(words) < 10:
+        continue
+    x = int(words[1])
+    y = int(words[2])
+    key = (x,y)
+    sectors[key] = words
+
+print()
+print()
+print(sectors)
+print()
+print()
+
+fout = open("sectors.p", "wb")
+pickle.dump(sectos, fout)
+fout.close()
